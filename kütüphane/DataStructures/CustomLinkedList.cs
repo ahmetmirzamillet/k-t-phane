32ad08761b78429ddf3b25968dc810c947064033
ahmetmirzamillet <milletmirza@gmail.com>
2025-12-30 12:28:22 +0300

    node ve linkedlist veri yapıları Implemente edildi

---
+++ kütüphane/DataStructures/CustomLinkedList.cs
@@ -0,0 +1,54 @@
+﻿namespace UniversityLibrarySystem.DataStructures
+{
+    public class CustomLinkedList<T>
+    {
+        private Node<T> head;
+
+        public CustomLinkedList()
+        {
+            head = null;
+        }
+
+        // 🔹 Listeye ekleme
+        public void Add(T data)
+        {
+            Node<T> newNode = new Node<T>(data);
+
+            if (head == null)
+            {
+                head = newNode;
+                return;
+            }
+
+            Node<T> current = head;
+            while (current.Next != null)
+            {
+                current = current.Next;
+            }
+
+            current.Next = newNode;
+        }
+
+        // 🔹 Listede var mı? (contains)
+        public bool Contains(T data)
+        {
+            Node<T> current = head;
+
+            while (current != null)
+            {
+                if (current.Data.Equals(data))
+                    return true;
+
+                current = current.Next;
+            }
+
+            return false;
+        }
+
+        // 🔹 Listeyi dolaşma (UI için)
+        public Node<T> GetHead()
+        {
+            return head;
+        }
+    }
+}
