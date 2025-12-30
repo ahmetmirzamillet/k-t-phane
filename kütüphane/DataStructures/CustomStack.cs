c805a62e6fa0f1432ca0b8af193658a1a2873cc8
ahmetmirzamillet <milletmirza@gmail.com>
2025-12-30 19:17:39 +0300

    Implemented custom Stack data structure for logging system

---
+++ kütüphane/DataStructures/CustomStack.cs
@@ -0,0 +1,56 @@
+﻿namespace UniversityLibrarySystem.DataStructures
+{
+    public class CustomStack<T>
+    {
+        private Node<T> top;
+        private int count;
+
+        public CustomStack()
+        {
+            top = null;
+            count = 0;
+        }
+
+        // 🔹 Stack'e ekleme (Push)
+        public void Push(T data)
+        {
+            Node<T> newNode = new Node<T>(data);
+            newNode.Next = top;
+            top = newNode;
+            count++;
+        }
+
+        // 🔹 Stack'ten çıkarma (Pop)
+        public T Pop()
+        {
+            if (IsEmpty())
+                return default;
+
+            T data = top.Data;
+            top = top.Next;
+            count--;
+            return data;
+        }
+
+        // 🔹 En üstteki eleman
+        public T Peek()
+        {
+            if (IsEmpty())
+                return default;
+
+            return top.Data;
+        }
+
+        // 🔹 Stack boş mu?
+        public bool IsEmpty()
+        {
+            return count == 0;
+        }
+
+        // 🔹 Log sayısı
+        public int Count()
+        {
+            return count;
+        }
+    }
+}
